cd76f464af2ae35e665f9ebaee0bbfdd3088c023
Emilio G. <hitsone.sccc@gmail.com>
2013-08-07 23:05:55 -0700

    Create Grades.java

---
+++ Grades.java
@@ -0,0 +1,16 @@
+package com.egarduno.GPAcalc;
+
+public class Grades {
+  private double gp;
+	private double Credits;
+
+	//SETS
+	public void setGP(double gp) {	this.gp = gp;}
+	public void setCredits(double Credits) {this.Credits = Credits;}
+	
+	//GETS
+	public double getGP() {return gp;}
+	public double getCredits() {return Credits;}
+
+
+}
