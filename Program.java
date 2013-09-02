363b4c6211f500acf6b12120bd2da68edf33f815
Emilio G. <hitsone.sccc@gmail.com>
2013-09-02 09:51:59 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,5 +1,12 @@
 package com.egarduno.GPAcalc;
 
+/*__________________________________
+
+	Emilio Garduno
+	08/07/2013
+	GPA Calculator
+__________________________________*/
+
 public class Program {
   @SuppressWarnings("unused")
 	public static void main(String args[]){
